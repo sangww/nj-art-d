66be517119105d27556e1204082f775f16140c76
Sang <sangww@gmail.com>
2024-02-23 14:56:08 -0800

    fix

--- app.js
+++ app.js
@@ -10,12 +10,7 @@ var Log = require('log'),
 
 var port = process.env.PORT || 3333;
 
-var ExpressPeerServer = require('peer').ExpressPeerServer;
-var peerApp = new peerExpress();
-var peerServer = require('http').createServer(peerApp);
 var options = { debug: true }
-var peerPort = 3366;
-peerApp.use('/peerjs', ExpressPeerServer(peerServer, options));
 
 app.use(express.static(__dirname + "/public"));
 
@@ -23,35 +18,7 @@ app.get('/',function(req,res){
   res.redirect('index.html');
 });
 
-io.on('connection',function(socket){
-
-  socket.on("join", (id) => {
-    console.log('new-connection:', id);
-    io.sockets.emit('new-connection', id);
-  });
-
-  /*
-  socket.on('server-id', (id) => {
-    serverid = id;
-    console.log('server-id:', serverid);
-    io.sockets.emit('server-id', serverid);
-  })
-  */
-
-  socket.on('message', (value) => handleMessage(value));
-  
-  function sendMessage(message) {
-    io.sockets.emit('message', message);
-  }
-  
-  function handleMessage(value) {
-    console.log("message:", value);
-    sendMessage(value);
-  }
-});
-
 http.listen(port,function(){
     log.info('info %s',port);
 });
 
-peerServer.listen(peerPort);
