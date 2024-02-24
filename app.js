0e27ec59ee95da2a7f67dcdf7725d0acd3f2b6c1
Sang <sangww@gmail.com>
2024-02-24 08:39:49 -0800

    fix

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 var express = require("express");
-var peerExpress = require('express');
 
 var app = new express ();
 var http = require("http").Server(app);
-var io = require("socket.io")(http);
 
-var Log = require('log'),
-    log = new Log('debug')
-
-var port = process.env.PORT || 3333;
-
-var options = { debug: true }
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
@@ -19,6 +12,4 @@ app.get('/',function(req,res){
 });
 
 http.listen(port,function(){
-    log.info('info %s',port);
 });
-
